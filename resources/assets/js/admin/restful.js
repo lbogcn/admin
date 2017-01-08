0965a04c15e231a1fb7f13b6f99e73fe0dfcff53
lenbo <664967337@qq.com>
2017-01-08 20:25:07 +0800

    loading效果

--- resources/assets/js/admin/restful.js
+++ resources/assets/js/admin/restful.js
@@ -1,32 +1,108 @@
-define(['ajax'], function(ajax) {
+define(['ajax', 'jquery'], function(ajax, $) {
+    var $backDrop;
+
+    function loading() {
+        $backDrop = $('<div class="modal-backdrop fade in" style="z-index: 9999;"></div>\
+        <div style="position: fixed;top: 50%; left: 50%; background: #FFF; border: 1px #000 solid;z-index: 10000; padding: 5px;">\
+            <img src="/images/loading.gif" alt="loading">加载中...\
+        </div>\
+        ');
+
+        $('body').append($backDrop);
+    }
+
+    function removeLoading() {
+        $backDrop.remove();
+    }
+
     return {
         deny: function(url) {
             if (confirm('确认禁用？')) {
-                ajax.apiPatch(url);
+                loading();
+
+                ajax.apiPatch(url).done(function() {
+                    alert('操作成功');
+                    window.location.reload();
+                }).fail(function (resp) {
+                    alert(resp.msg);
+                }).always(function () {
+                    removeLoading();
+                });
             }
         },
         restore: function(url) {
             if (confirm('确认恢复？')) {
-                ajax.apiPatch(url);
+                loading();
+
+                ajax.apiPatch(url).done(function() {
+                    alert('操作成功');
+                    window.location.reload();
+                }).fail(function (resp) {
+                    alert(resp.msg);
+                }).always(function () {
+                    removeLoading();
+                });
             }
         },
         get: function (url, params) {
-            return ajax.apiGet(url, params);
+            loading();
+
+            return ajax.apiGet(url, params).fail(function (resp) {
+                alert(resp.msg);
+            }).always(function () {
+                removeLoading();
+            });
         },
         post: function (url, params) {
-            ajax.apiPost(url, params);
+            loading();
+
+            ajax.apiPost(url, params).done(function() {
+                alert('操作成功');
+                window.location.reload();
+            }).fail(function (resp) {
+                alert(resp.msg);
+            }).always(function () {
+                removeLoading();
+            });
         },
         put: function(url, params) {
-            ajax.apiPut(url, params);
+            loading();
+
+            ajax.apiPut(url, params).done(function() {
+                alert('操作成功');
+                window.location.reload();
+            }).fail(function (resp) {
+                alert(resp.msg);
+            }).always(function () {
+                removeLoading();
+            });
         },
         patch: function(url, params) {
+            loading();
+
             if (confirm('是否继续？')) {
-                ajax.apiPatch(url, params);
+                ajax.apiPatch(url, params).done(function() {
+                    alert('操作成功');
+                    window.location.reload();
+                }).fail(function (resp) {
+                    alert(resp.msg);
+                }).always(function () {
+                    removeLoading();
+                });
             }
         },
         del: function(url) {
+            loading();
+
             if (confirm('确认删除？')) {
-                ajax.apiDelete(url);
+                ajax.apiDelete(url).done(function() {
+                    alert('操作成功');
+                    window.location.reload();
+                }).fail(function (resp) {
+                    alert(resp.msg);
+                }).always(function () {
+                    removeLoading();
+                });
             }
         }
     };
