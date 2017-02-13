a6ae950ffcd20022a4462b3ab7867cfc991cf63d
lenbo <664967337@qq.com>
2017-02-13 21:43:04 +0800

    gitignore

---
+++ public/js/require-config.js
@@ -0,0 +1,19 @@
+requirejs.config({
+    paths: {
+        'jquery': '/plugins/jquery/jquery.min',
+        'bootstrap': '/plugins/bootstrap/js/bootstrap.min',
+        'metisMenu': '/plugins/metisMenu/metisMenu.min',
+        'utils': '/js/utils',
+        'ajax': '/js/ajax'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'metisMenu': {
+            deps: ['bootstrap']
+        }
+    }
+});
+
+//# sourceMappingURL=require-config.js.map
