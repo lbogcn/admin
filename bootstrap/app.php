1046cd618d10e17c4eef4e6e04fba46fc72f918e
lenbo <664967337@qq.com>
2017-02-15 21:53:00 +0800

    修改env文件

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
+$app->useEnvironmentPath('/home/www/');
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
