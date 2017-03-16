97469dbf51df59f03e82ad100f0f36b321bc940e
bo.lin <bo.lin@etcchebao.com>
2017-03-16 12:47:58 +0800

    文章摘要优化

--- functions.php
+++ functions.php
@@ -23,3 +23,15 @@ function cdn($path) {
 
     return '//' . config('domain.cdn') . $separator . $path;
 }
+
+/**
+ * 生成字符串摘要
+ * @param $str
+ * @param $len
+ * @return string
+ */
+function str_excerpt($str, $len) {
+    $str = trim(strip_tags($str));
+
+    return str_limit($str, $len);
+}
