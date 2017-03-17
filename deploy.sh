9725b8f78fbaf7a047d50f8389423ef1c3bfcbfa
bo.lin <bo.lin@etcchebao.com>
2017-03-17 18:20:43 +0800

    更新session目录

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# 判断session目录是否存在
+if [ ! -d "/tmp/lbog.cn/sessions" ]; then
+  mkdir -p /tmp/lbog.cn/sessions
+fi
+
 php artisan optimize --force
 php artisan config:cache
 php artisan route:cache
