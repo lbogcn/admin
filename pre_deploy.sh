8f5bb98af25422c7b91006777f3d2e512f516259
Lenbo <664967337@qq.com>
2017-06-18 14:34:25 +0800

    debug 错误单词

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -2,4 +2,4 @@
 # 在部署代码之前的准备工作，如git的一些前置检查、vendor的安装（更新）
 
 cp -r /home/www/deploy/prod/framework/vendor ./vendor
-cp -r /home/www/deploy/prod/framework/BaseComponents/* ./app/Compoments/Base
+cp -r /home/www/deploy/prod/framework/BaseComponents/* ./app/Components/Base
